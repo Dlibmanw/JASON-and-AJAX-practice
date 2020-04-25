a414e87c009241a419aca6a82447e4effbf5ee4a
Dafna Libman <dafnalibman@gmail.com>
2020-04-25 21:24:38 +0100

    Update the url in the AJAX request, add pageCounter variable to url to make it dynamic

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
+var pageCounter = 1;
 var animalContainer = document.getElementById('animal-info')
 var btn = document.getElementById('btn');
 btn.addEventListener('click', function(){
   var ourRequest = new XMLHttpRequest();
-  ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json')
+  ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json')
   ourRequest.onload = function() {
     var ourData = JSON.parse(ourRequest.responseText);
     renderHTML(ourData)
   };
   ourRequest.send();
+  pageCounter++; 
 });
 
 function renderHTML(data){

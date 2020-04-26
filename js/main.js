a341eed7a124077ce91e8f55ef976414f7b5edb9
Dafna Libman <dafnalibman@gmail.com>
2020-04-26 12:58:50 +0100

    Add css class to allow hiding the button after 3 clicks, output more data about liked and disliked food

--- js/main.js
+++ js/main.js
@@ -5,17 +5,38 @@ btn.addEventListener('click', function(){
   var ourRequest = new XMLHttpRequest();
   ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json')
   ourRequest.onload = function() {
-    var ourData = JSON.parse(ourRequest.responseText);
+    if (ourRequest.status >= 200 && ourRequest.status < 400 ){
+      var ourData = JSON.parse(ourRequest.responseText);
     renderHTML(ourData)
+    } else {
+      console.log("We connected to the server but it returned an error.")
+    }
   };
   ourRequest.send();
   pageCounter++; 
+  if (pageCounter > 3) {
+    btn.classList.add('hide-me')
+  }
 });
 
 function renderHTML(data){
   var htmlString = "";
   for (i = 0; i < data.length; i++){
-    htmlString += "<p>" + data[i].name + " is a " + data[i].species + ".</p>"
+    htmlString += "<p>" + data[i].name + " is a " + data[i].species + " he likes eating ";
+    for (ii = 0; ii < data[i].foods.likes.length; ii++){
+      if (ii == 0){
+        htmlString += data[i].foods.likes[ii]
+      }
+      else htmlString += " and " + data[i].foods.likes[ii]
+    }
+    htmlString += " and he doesn't like eating ";
+    for (ii = 0; ii < data[i].foods.dislikes.length; ii++){
+      if (ii == 0){
+        htmlString += data[i].foods.dislikes[ii]
+      }
+      else htmlString += " and " + data[i].foods.dislikes[ii]
+    }
+    htmlString += ".</p>";
   }
   animalContainer.insertAdjacentHTML('beforeend', htmlString);
 } 
